370b9152b2873435eaa7e8d89703cd44172e305e
ownzyuan <1076949424@qq.com>
2021-01-22 17:23:01 +0800

    完善

---
+++ springcloud-api/src/main/java/com/zy/pojo/Dept.java
@@ -0,0 +1,9 @@
+package com.zy.pojo;
+
+import java.io.Serializable;
+
+public class Dept implements Serializable {
+    private Integer id;
+    private String name;
+    private String dbSource;
+}
