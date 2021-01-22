690a8be2ee51dcd7cf69801bdcd6fda4ee115322
ownzyuan <1076949424@qq.com>
2021-01-22 17:59:23 +0800

    新增提供者板块

--- springcloud-api/src/main/java/com/zy/pojo/Dept.java
+++ springcloud-api/src/main/java/com/zy/pojo/Dept.java
@@ -1,7 +1,14 @@
 package com.zy.pojo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.io.Serializable;
 
+@Data
+@NoArgsConstructor
+@Accessors(chain = true)
 public class Dept implements Serializable {
     private Integer id;
     private String name;
