082b79e9834873450a0f65b83278901d79730ecb
ownzyuan <1076949424@qq.com>
2021-01-22 17:28:22 +0800

    完善

--- springcloud-api/src/main/java/com/zy/pojo/Dept.java
+++ springcloud-api/src/main/java/com/zy/pojo/Dept.java
@@ -5,5 +5,10 @@ import java.io.Serializable;
 public class Dept implements Serializable {
     private Integer id;
     private String name;
+    // 数据库名
     private String dbSource;
+
+    public Dept(String name) {
+        this.name = name;
+    }
 }
