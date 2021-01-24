6fee647dc5abcfba6873bd34d7c22a6dc0cd9040
ownzyuan <1076949424@qq.com>
2021-01-24 18:50:54 +0800

    provider成为Eureka-client

--- springcloud-provider-dept-8001/src/main/java/com/zy/springcloud/DeptProviderApplication.java
+++ springcloud-provider-dept-8001/src/main/java/com/zy/springcloud/DeptProviderApplication.java
@@ -2,8 +2,11 @@ package com.zy.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+// 开启EurekaClient，启动后自动在eureka中注册
+@EnableEurekaClient
 public class DeptProviderApplication {
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderApplication.class, args);
