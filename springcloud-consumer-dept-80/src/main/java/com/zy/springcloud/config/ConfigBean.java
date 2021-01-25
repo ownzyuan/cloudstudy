49d886790bacb17142dd34125f03ae66b342dae6
ownzyuan <1076949424@qq.com>
2021-01-25 23:44:27 +0800

    consumer引入Ribbon

--- springcloud-consumer-dept-80/src/main/java/com/zy/springcloud/config/ConfigBean.java
+++ springcloud-consumer-dept-80/src/main/java/com/zy/springcloud/config/ConfigBean.java
@@ -1,5 +1,6 @@
 package com.zy.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,9 +8,15 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
 
+    /*@Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }*/
+
+    // 配置负载均衡实现RestTemplate
     @Bean
+    @LoadBalanced // Ribbon
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
     }
-
 }
